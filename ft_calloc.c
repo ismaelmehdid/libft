56665aa0eb5143f4c6fca0cc14b1cf568e894bfa
ismaelmehdid <ismaelmehdidwork@gmail.com>
2023-11-03 23:25:13 +0100

    adding all the rest

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,26 +6,11 @@
 /*   By: imehdid <imehdid@student.42angouleme.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/01 23:26:57 by imehdid           #+#    #+#             */
-/*   Updated: 2023/11/02 00:35:24 by imehdid          ###   ########.fr       */
+/*   Updated: 2023/11/03 20:21:20 by imehdid          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-void	ft_bzero(void *s, size_t n)
-{
-	size_t	i;
-	char	*str;
-
-	i = 0;
-	str = s;
-	while (i < n)
-	{
-		*str = '\0';
-		str++;
-		i++;
-	}
-}
 
 void	*ft_calloc(size_t elementCount, size_t elementSize)
 {
--- ft_striteri.c
+++ ft_striteri.c
@@ -6,7 +6,20 @@
 /*   By: imehdid <imehdid@student.42angouleme.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/03 16:10:14 by imehdid           #+#    #+#             */
-/*   Updated: 2023/11/03 19:38:07 by imehdid          ###   ########.fr       */
+/*   Updated: 2023/11/03 21:08:38 by imehdid          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+void	ft_striteri(char *s, void (*f)(unsigned int, char*))
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		(*f)(i, str + i);
+		i++;
+	}
+}
