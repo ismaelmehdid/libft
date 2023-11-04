09e2c24b13b43803e950fb9a446630f35928a156
ismaelmehdid <ismaelmehdidwork@gmail.com>
2023-11-04 20:38:34 +0100

    fixing strrchr (errors on the cast of searchedchar and condition to check if there is \0 as a first character in string searched)

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: imehdid <imehdid@student.42angouleme.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/01 23:28:41 by imehdid           #+#    #+#             */
-/*   Updated: 2023/11/02 00:39:05 by imehdid          ###   ########.fr       */
+/*   Updated: 2023/11/04 20:35:13 by imehdid          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,19 @@ char	*ft_strrchr(const char *string, int searchedChar)
 	int	i;
 
 	i = 0;
+	if (!string)
+		return (NULL);
 	while (string[i])
 		i++;
 	string += i;
 	while (i != 0)
 	{
-		if (*string == searchedChar)
+		if (*string == (char)searchedChar)
 			return ((char *)string);
 		string--;
 		i--;
 	}
+	if (*string == (char)searchedChar)
+		return ((char *)string);
 	return (NULL);
 }
