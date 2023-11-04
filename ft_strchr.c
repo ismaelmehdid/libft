2042c445f61ace7faf3187ea93b1fe818b6dd8d6
ismaelmehdid <ismaelmehdidwork@gmail.com>
2023-11-04 20:10:36 +0100

    fixed strchr (error on the cast of searchedChar) + norminette fix

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: imehdid <imehdid@student.42angouleme.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/01 23:28:07 by imehdid           #+#    #+#             */
-/*   Updated: 2023/11/04 20:07:47 by imehdid          ###   ########.fr       */
+/*   Updated: 2023/11/04 20:10:03 by imehdid          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,6 @@ char	*ft_strchr(const char *string, int searchedChar)
 		string++;
 	}
 	if (*string == (char)searchedChar)
-		return((char *)(string));
+		return ((char *)(string));
 	return (NULL);
 }
