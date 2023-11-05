328023e1f1d67727cde0dc380b1bacefd917694e
ismaelmehdid <ismaelmehdidwork@gmail.com>
2023-11-05 22:03:28 +0100

    fixed calloc (error on passing the wrong bytes size in bzero function)

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,17 +6,19 @@
 /*   By: imehdid <imehdid@student.42angouleme.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/01 23:26:57 by imehdid           #+#    #+#             */
-/*   Updated: 2023/11/03 20:21:20 by imehdid          ###   ########.fr       */
+/*   Updated: 2023/11/05 22:01:53 by imehdid          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_calloc(size_t elementCount, size_t elementSize)
+void	*ft_calloc(size_t elementcount, size_t elementsize)
 {
 	void	*array;
 
-	array = malloc(elementCount * elementSize);
-	ft_bzero(array, elementCount);
+	array = malloc(elementcount * elementsize);
+	if (array == NULL)
+		return (NULL);
+	ft_bzero(array, (elementcount * elementsize));
 	return (array);
 }
