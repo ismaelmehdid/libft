63ddebb6f5818ef25e7230ffad091b95cc64590d
ismaelmehdid <ismaelmehdidwork@gmail.com>
2023-11-06 00:09:34 +0100

    fixing exeptions on memcpy (it has to segfault when source is null)

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: imehdid <imehdid@student.42angouleme.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/01 23:27:52 by imehdid           #+#    #+#             */
-/*   Updated: 2023/11/04 02:06:36 by imehdid          ###   ########.fr       */
+/*   Updated: 2023/11/06 00:07:53 by imehdid          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,10 @@ void	*ft_memcpy(void *destination, const void *source, size_t size)
 	size_t	i;
 
 	i = 0;
+	if (!destination && !source && size > 0)
+		return (NULL);
+	if (size == 0)
+		return (destination);
 	while (i < size)
 	{
 		*(unsigned char *)(destination) = *(unsigned char *)(source);
@@ -24,5 +28,5 @@ void	*ft_memcpy(void *destination, const void *source, size_t size)
 		destination++;
 		i++;
 	}
-	return (destination);
+	return (destination - i);
 }
