2a0bbd87f8ac8e2e1fa6e187e532869c165c6e39
ismaelmehdid <ismaelmehdidwork@gmail.com>
2023-11-06 16:35:07 +0100

    fixed memmove and now handling memory overlap and some edge cases

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: imehdid <imehdid@student.42angouleme.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/01 23:27:57 by imehdid           #+#    #+#             */
-/*   Updated: 2023/11/04 18:49:10 by imehdid          ###   ########.fr       */
+/*   Updated: 2023/11/06 16:33:50 by imehdid          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,27 +14,29 @@
 
 void	*ft_memmove(void *destination, const void *source, size_t size)
 {
-	int	i;
+	size_t	i;
 
+	i = 0;
+	if (size == 0)
+		return (destination);
+	if (!destination && !source && size > 0)
+		return (destination);
 	if (destination > source)
 	{
-		i = (int)size--;
-		while (i >= 0)
+		i = (int)size - 1;
+		while (i < size)
 		{
 			*(unsigned char *)(destination + i)
 				= *(unsigned char *)(source + i);
 			i--;
 		}
+		return (destination);
 	}
-	else
+	while (i < size)
 	{
-		i = (int)size;
-		while (i >= 0)
-		{
-			*(unsigned char *)(destination + i)
-				= *(unsigned char *)(source + i);
-			i--;
-		}
+		*(unsigned char *)(destination + i)
+			= *(unsigned char *)(source + i);
+		i++;
 	}
 	return (destination);
 }
