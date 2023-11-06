f2fa4d62141f1f0ecfed9a830cb104a81d17ad03
ismaelmehdid <ismaelmehdidwork@gmail.com>
2023-11-06 23:04:20 +0100

    fixed strlcat ?

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,27 +6,33 @@
 /*   By: imehdid <imehdid@student.42angouleme.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/01 23:28:18 by imehdid           #+#    #+#             */
-/*   Updated: 2023/11/02 00:36:05 by imehdid          ###   ########.fr       */
+/*   Updated: 2023/11/06 23:03:49 by imehdid          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
 size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
-	size_t	e;
-	size_t	j;
+	int	i;
+	int	e;
+	int	lendst;
+	int	lensrc;
 
+	if (size == 0)
+		return (0);
+	i = 0;
 	e = 0;
-	j = 0;
-	while (dst[e])
-		e++;
-	while ((e + j) < size)
+	lendst = ft_strlen(dst);
+	lensrc = ft_strlen(src);
+	i += lendst;
+	while (i < (int)size && src[e])
 	{
-		if (src[j] == '\0')
-			return (j + e);
-		dst[e + j] = src[j];
-		j++;
+		dst[i] = src[e];
+		e++;
+		i++;
 	}
-	return (j + e);
+	dst[i] = '\0';
+	return (lensrc + lendst);
 }
