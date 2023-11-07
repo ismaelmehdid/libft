1c150b205b5a7d93bbe829d9f5fb2e74a938a0cd
ismaelmehdid <ismaelmehdidwork@gmail.com>
2023-11-08 00:44:56 +0100

    fixed calloc so it handle negative passed values

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: imehdid <imehdid@student.42angouleme.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/01 23:26:57 by imehdid           #+#    #+#             */
-/*   Updated: 2023/11/05 22:01:53 by imehdid          ###   ########.fr       */
+/*   Updated: 2023/11/08 00:44:26 by imehdid          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ void	*ft_calloc(size_t elementcount, size_t elementsize)
 {
 	void	*array;
 
+	if ((int)elementcount < 0 && (int)elementsize < 0)
+		return (NULL);
 	array = malloc(elementcount * elementsize);
 	if (array == NULL)
 		return (NULL);
