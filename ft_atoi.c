e90700526c1cd605995d2cf46ea6854afbf61eb5
ismaelmehdid <ismaelmehdidwork@gmail.com>
2023-11-07 21:57:45 +0100

    fixed atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: imehdid <imehdid@student.42angouleme.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/01 23:25:41 by imehdid           #+#    #+#             */
-/*   Updated: 2023/11/05 22:30:00 by imehdid          ###   ########.fr       */
+/*   Updated: 2023/11/07 21:57:30 by imehdid          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,17 +25,17 @@ int	ft_atoi(const char *str)
 		|| str[i] == '+' || str[i] == '-')
 	{
 		if ((str[i] == '+' || str[i] == '-')
-			&& ((str[i + 1] == '+' || str[i + 1] == '-')
-				|| ((str[i + 1] >= 9 && str[i + 1] <= 13) || str[i + 1] == 32)))
+			&& (str[i + 1] == '+' || str[i + 1] == '-'
+				|| (str[i + 1] >= 9 && str[i + 1] <= 13) || str[i + 1] == 32))
 			return (0);
+		if (str[i] == '-')
+			countm++;
 		i++;
 	}
-	if (str[i - 1] == '-')
-		countm++;
+	if ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
+		return (0);
 	while (str[i] >= '0' && str[i] <= '9')
-	{
 		temp = temp * 10 + str[i++] - '0';
-	}
 	if (countm == 1)
 		return (-temp);
 	return (temp);
