5c5840c78021281f1555e32edd40444b8b3949c1
ismaelmehdid <ismaelmehdidwork@gmail.com>
2023-11-07 17:47:43 +0100

    fixed all strlcat and handling every case rn

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,33 +6,34 @@
 /*   By: imehdid <imehdid@student.42angouleme.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/01 23:28:18 by imehdid           #+#    #+#             */
-/*   Updated: 2023/11/06 23:03:49 by imehdid          ###   ########.fr       */
+/*   Updated: 2023/11/07 17:47:06 by imehdid          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
 
 size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
-	int	i;
-	int	e;
-	int	lendst;
-	int	lensrc;
+	size_t	sizedst;
+	size_t	sizesrc;
+	size_t	i;
+	size_t	e;
 
-	if (size == 0)
-		return (0);
 	i = 0;
 	e = 0;
-	lendst = ft_strlen(dst);
-	lensrc = ft_strlen(src);
-	i += lendst;
-	while (i < (int)size && src[e])
-	{
-		dst[i] = src[e];
-		e++;
-		i++;
-	}
+	if (size == 0 && dst == NULL)
+		return ((size_t)ft_strlen(src));
+	sizedst = (size_t)ft_strlen(dst);
+	sizesrc = (size_t)ft_strlen(src);
+	if (size == 0)
+		return (sizesrc);
+	if (size < sizedst)
+		return (sizesrc + size);
+	if (size == sizedst)
+		return (sizedst + sizesrc);
+	i += sizedst;
+	while ((size - sizedst - 1) > e && src[e])
+		dst[i++] = src[e++];
 	dst[i] = '\0';
-	return (lensrc + lendst);
+	return (sizedst + sizesrc);
 }
