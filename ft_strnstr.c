5331c31623bcd5ff972444e419b5395947b48e4d
ismaelmehdid <ismaelmehdidwork@gmail.com>
2023-11-08 00:02:46 +0100

    fixed strnstr and handling now negative passed value

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,37 +6,35 @@
 /*   By: imehdid <imehdid@student.42angouleme.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/01 23:28:35 by imehdid           #+#    #+#             */
-/*   Updated: 2023/11/07 18:01:50 by imehdid          ###   ########.fr       */
+/*   Updated: 2023/11/08 00:02:11 by imehdid          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
 char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
 	int	e;
 	int	j;
 
+	j = 0;
 	e = 0;
-	j = ft_strlen(little);
+	if ((int)len < 0)
+		len = (size_t)ft_strlen(big);
 	if ((big == NULL && len == 0) || (ft_strlen(little) > ft_strlen(big)))
 		return (NULL);
 	if (!*little)
 		return ((char *)big);
-	while (*big && len != 0)
+	while (big[e] && e < (int)len)
 	{
-		if (*big == little[0])
-		{
-			while (*big == little[e++] && len-- != 0)
-			{
-				if (e == j)
-					return ((char *)(big - e + 1));
-				big++;
-			}
-		}
-		len--;
-		big++;
-		e = 0;
+		j = 0;
+		while (big[j + e] && little[j] && e + j < (int)len
+			&& big[j + e] == little[j])
+			j++;
+		if (little[j] == '\0')
+			return ((char *)(big + e));
+		e++;
 	}
 	return (NULL);
 }
